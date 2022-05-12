f95e7b79cc0a451043eaa73269804e5cf9a03812
Robson Costa <telex.r3costa@gmail.com>
2022-05-12 03:39:24 -0300

    Conclusão do Projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
+import { TransferenciaService } from './services/transferencia.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'bytebank';
-  transferencias: any[] = [];
+  // transferencias: any[] = [];
 
-  transferir($event: any){
-    console.log($event)
-    const transferencia = {...$event, data: new Date}
-    this.transferencias.push(transferencia);
-  }
+  constructor(private service: TransferenciaService){};
+
+  // transferir($event: any){
+  //   // console.log($event)
+  //   // const transferencia = {...$event, data: new Date}
+  //   // this.transferencias.push(transferencia);
+  //   this.service.adicionar($event);
+  //   // no html >>> <app-nova-transferencia (aoTransferir) = "transferir($event)"></app-nova-transferencia>
+  // }
+
+  // no html >> <app-nova-transferencia></app-nova-transferencia> para trazer esta "Entidade"
+
+  // no html >>> <app-extrato></app-extrato> para trazer esta "Entidade"
 
 }
--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,7 @@
+import { Transferencia } from './../models/transferencia.model';
+import { TransferenciaService } from './../services/transferencia.service';
 import { Component, EventEmitter, Output } from "@angular/core";
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-nova-transferencia',
@@ -13,13 +16,24 @@ export class NovaTransferenciaComponent {
   valor!: number;
   destino!: number;
 
+  constructor(private service: TransferenciaService, private router: Router) {}
+
   transferir() {
     console.log('Solicitada nova Transferência');
     console.log('Valor: ', this.valor);
     console.log('Destino:', this.destino);
 
-    const valorEmitir = {valor:this.valor, destino:this.destino}
-    this.aoTransferir.emit(valorEmitir);
+    // const valorEmitir = {valor:this.valor, destino:this.destino}
+    // this.aoTransferir.emit(valorEmitir);
+    const valorEmitir: Transferencia = {valor:this.valor, destino:this.destino}
+    this.service.adicionar(valorEmitir).subscribe(
+      (resultado) => {
+        console.log(resultado);
+        this.limparCampos();
+        this.router.navigateByUrl('extrato');
+      },
+      (error) => console.error(error)
+    )
 
     this.limparCampos();
   }
